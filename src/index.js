05dedcbf18cd320e86848ba0f0a377958c65fe90
MARIO CESAR FREIRE DIAS FILHO <mario.dias@tce.ce.gov.br>
2025-04-01 17:24:15 -0300

    feat: add the remaining files to git

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import './polyfills';
-// ... resto do seu código 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Se existir
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+// Use o método de renderização mais simples
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+); 
